6cb4841368fb36bb39f73214e7fd1cce54222b7b
mx5566 <2473321894@qq.com>
2024-04-18 23:03:58 +0800

    Update build.bat

--- build.bat
+++ build.bat
@@ -1,6 +1,6 @@
 @echo off
 set dir=%~dp0
-cd %dir%
+cd %dir%/server
 @rem 设置环境变量
 set GOPROXY=https://goproxy.cn,direct
 
@@ -10,7 +10,9 @@ go version
 @rem 编译程序
 go build
 
-copy  /y server.exe  .\release
+copy  /y server.exe  ..\release
+
+del /F /Q  server.exe
 
 
 
