af9bdf4b3df1ec11375d341be2e0331873937ae0
mengxiang <2473321894@qq.com>
2024-06-04 22:06:02 +0800

    update

--- copy_to_mount_dir.sh
+++ copy_to_mount_dir.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
 # 拷贝资源文件配置文件
-cp 
+cp -afr release/maps /data/server 
+cp -afr release/table /data/server
+cp -afr release/*.sh /data/server
+cp -afr release/config.yaml /data/server
 
